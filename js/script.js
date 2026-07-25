76e607407f645eeabf1aa4a2f850cab57d13a675
Bilalijaz003 <bilalijaz002@gmail.com>
2026-07-26 01:39:20 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ async function analyzeImage() {
     try {
 
         // Send to Flask backend
-        const response = await fetch('http://localhost:5000/predict', {
+        const response = await fetch('https://cropguard-ai-backend.onrender.com', {
             method: 'POST',
             body: formData
         });
